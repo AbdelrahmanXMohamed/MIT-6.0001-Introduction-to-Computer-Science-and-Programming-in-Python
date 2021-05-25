b755b25f31e216485c17b9fa03530629c9e24688
AbdelrahmanXMohamed <81911743+AbdelrahmanXMohamed@users.noreply.github.com>
2021-05-25 07:52:36 +0200

    Add files via upload

---
+++ ps0/ps0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 25 06:32:25 2021
+
+@author: Xbenx
+"""
+
+import numpy as np
+X = int(input("Enter number X: "))
+Y = int(input("Enter number Y: "))
+print(f"X**Y = {X**Y}\nlog(X) = {np.log2(X)}\n ")
